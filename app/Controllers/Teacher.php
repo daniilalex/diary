75289dca106ec5a081b184b9e7305c8287cd7adb
danalex <daniil.alex1@gmail.com>
2022-05-18 23:33:30 +0300

    make teacher controller. take teacher id from session. take teachers students.

--- app/Controllers/Teacher.php
+++ app/Controllers/Teacher.php
@@ -3,19 +3,37 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\TeacherModel;
+use App\Models\ClassModel;
 
 class Teacher extends BaseController
 {
+    public $teachers;
+    public $classes;
+
     public function __construct()
     {
-        //take from session user key and compare it with array element
-        if(session()->user['type'] != 'teacher'){
-            dd('can enter only the teacher');
+        $this->teachers = new TeacherModel();
+        $this->classes = new ClassModel();
+
+        if (session()->user['type'] != 'teacher') {
+            dd((array)'Allowed only teacher');
         }
     }
 
     public function index()
     {
-        return view('users/teacher');
+        //take a teacher id from session
+        $teacher = $this->teachers->where('user_id', session()->user['id'])->first();
+//get students by teachers class id with created function getStudents
+        $data = [
+            'students' => $this->classes->getStudents($teacher['class_id']),
+        ];
+//if teacher class_id isn't null add to the data new key class with teacher class_id
+        if ($teacher['class_id'] != null) {
+            $data['class'] = $this->classes->find($teacher['class_id']);
+        }
+
+        return view('users/teacher/index', $data);
     }
 }
