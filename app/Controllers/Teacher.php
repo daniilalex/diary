5930ebd9ee74531e40d8f31f9fabcb2ebd67e5b4
danalex <daniil.alex1@gmail.com>
2022-05-24 22:57:18 +0300

    add validation rules. add function createTimeTable.

--- app/Controllers/Teacher.php
+++ app/Controllers/Teacher.php
@@ -7,15 +7,14 @@ use App\Models\StudentModel;
 use App\Models\TeacherModel;
 use App\Models\ClassModel;
 use App\Models\LessonModel;
-use App\Models\TimeTable;
-use App\Controllers\Director;
+use App\Models\TimeTableModel;
 
 class Teacher extends BaseController
 {
     public $teachers;
     public $classes;
     public $lessons;
-    public $time_table;
+    public $schedules;
     public $students;
 
 
@@ -25,6 +24,7 @@ class Teacher extends BaseController
         $this->students = new StudentModel();
         $this->classes = new ClassModel();
         $this->lessons = new LessonModel();
+        $this->schedules = new TimeTableModel();
 
 
 
@@ -38,15 +38,18 @@ class Teacher extends BaseController
         //take a teacher id from session
         $teacher = $this->teachers->where('user_id', session()->user['id'])->first();
         $data = [
-            'students' => $this->classes->getStudents(),
-            'days' => TimeTable::DAYS,
+            'students' => $this->classes->getStudents($teacher['class_id']),
+            'days' => TimeTableModel::DAYS,
             'teachers' => $this->teachers
                 ->select('teachers.id, users.email, users.firstname, users.lastname, lessons.title as lesson')
                 ->join('users', 'users.id = teachers.user_id')
-                ->join('lessons', 'lessons.id = teachers.lesson_id')
-                ->where('lesson_id', 0)
-                ->findAll()
-
+                ->join('lessons', 'lessons.id = teachers.lesson_id', 'left')
+                ->where('lesson_id !=', 0)
+                ->findAll(),
+            'errors' => $this->session->getFlashdata('errors') ?? null,
+            'success' => $this->session->getFlashdata('success') ?? null,
+            'schedule' => TimeTableModel::getLessons($teacher['class_id']),
+            'count_lessons' => $this->schedules->where('class_id', $teacher['class_id'])->countAll()
         ];
 
 //if teacher class_id isn't null add to the data new key class with teacher class_id
@@ -59,15 +62,41 @@ class Teacher extends BaseController
 
     public function createTimeTable()
     {
-        $timetable_data = [
-            'lesson_number' => $this->request->getPost('lesson_number'),
-            'cabinet' => $this->request->getPost('cabinet'),
-            'class_id' => $this->request->getVar('class_id'),
-            'lesson_id' => $this->request->getVar('lesson_id'),
-            'teacher_id' => $this->request->getVar('teacher_id'),
-        ];
-        $this->time_table->insert($timetable_data);
+        if ($this->validate([
+            //in_list - must be chosen from select(list) + convert array to string
+            'week_day' => 'required|in_list[' . implode(',', TimeTableModel::DAYS) . ']',
+            //Fails if field is not exactly the parameter
+            //value.
+            'lesson_number' => 'required|integer|exact_length[1]',
+            //checks from DB values, put method isNotUnique to table teachers.id
+            'teacher_id' => 'required|is_not_unique[teachers.id]',
+            'cabinet' => 'required|string|min_length[1]|max_length[30]',
+        ])) {
+            $schedule = $this->schedules
+                ->where('week_day', $this->request->getVar('week_day'))
+                ->where('lesson_number', $this->request->getVar('lesson_number'))
+                ->first();
+            if (!$schedule) {
+                $user = $this->teachers->where('user_id', session()->user['id'])->first();
+                $class_id = $user['class_id'];
+                $teacher = $this->teachers->where('id', $this->request->getVar('teacher_id'))->first();
 
-        return redirect()->to(base_url('teacher/index'));
+                $schedule_data = [
+                    'class_id' => $class_id,
+                    'lesson_number' => $this->request->getVar('lesson_number'),
+                    'lesson_id' => $teacher['lesson_id'],
+                    'teacher_id' => $teacher['id'],
+                    'cabinet' => $this->request->getVar('cabinet'),
+                    'week_day' => $this->request->getVar('week_day'),
+                ];
+                $this->schedules->insert($schedule_data);
+                return redirect()->to(base_url('/teacher/index'))->with('success', 'Lesson is successfully added to schedule');
+            } else {
+                $errors = 'Time is used';
+            }
+        } else {
+            $errors = $this->validator->listErrors();
+        }
+        return  redirect()->to(base_url('/teacher/index'))->with('errors', $errors);
     }
 }
