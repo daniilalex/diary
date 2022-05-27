9fcb94749b015c1981383507cbc253b5e703de90
danalex <daniil.alex1@gmail.com>
2022-05-28 01:16:06 +0300

    add new get students function

--- app/Models/TimeTableModel.php
+++ app/Models/TimeTableModel.php
@@ -6,15 +6,15 @@ use CodeIgniter\Model;
 
 class TimeTableModel extends Model
 {
-    protected $DBGroup          = 'default';
-    protected $table            = 'timetable';
-    protected $primaryKey       = 'id';
+    protected $DBGroup = 'default';
+    protected $table = 'timetable';
+    protected $primaryKey = 'id';
     protected $useAutoIncrement = true;
-    protected $insertID         = 0;
-    protected $returnType       = 'array';
-    protected $useSoftDeletes   = false;
-    protected $protectFields    = true;
-    protected $allowedFields    =  [
+    protected $insertID = 0;
+    protected $returnType = 'array';
+    protected $useSoftDeletes = false;
+    protected $protectFields = true;
+    protected $allowedFields = [
         'class_id',
         'lesson_number',
         'lesson_id',
@@ -25,27 +25,27 @@ class TimeTableModel extends Model
 
     // Dates
     protected $useTimestamps = false;
-    protected $dateFormat    = 'datetime';
-    protected $createdField  = 'created_at';
-    protected $updatedField  = 'updated_at';
-    protected $deletedField  = 'deleted_at';
+    protected $dateFormat = 'datetime';
+    protected $createdField = 'created_at';
+    protected $updatedField = 'updated_at';
+    protected $deletedField = 'deleted_at';
 
     // Validation
-    protected $validationRules      = [];
-    protected $validationMessages   = [];
-    protected $skipValidation       = false;
+    protected $validationRules = [];
+    protected $validationMessages = [];
+    protected $skipValidation = false;
     protected $cleanValidationRules = true;
 
     // Callbacks
     protected $allowCallbacks = true;
-    protected $beforeInsert   = [];
-    protected $afterInsert    = [];
-    protected $beforeUpdate   = [];
-    protected $afterUpdate    = [];
-    protected $beforeFind     = [];
-    protected $afterFind      = [];
-    protected $beforeDelete   = [];
-    protected $afterDelete    = [];
+    protected $beforeInsert = [];
+    protected $afterInsert = [];
+    protected $beforeUpdate = [];
+    protected $afterUpdate = [];
+    protected $beforeFind = [];
+    protected $afterFind = [];
+    protected $beforeDelete = [];
+    protected $afterDelete = [];
 
     const MONDAY = 'monday';
     const TUESDAY = 'tuesday';
@@ -60,6 +60,7 @@ class TimeTableModel extends Model
         self::THURSDAY,
         self::FRIDAY,
     ];
+
 //static function can access methods and properties of a class and could be invoked directly outside class by using scope ::
     static public function getLessons(int $class_id)
     {
@@ -77,22 +78,37 @@ class TimeTableModel extends Model
 
         return $response;
     }
+
     //get all lessons by teacher_id by chosen date
-    public function getTeacherLessons(int $teacher_id, string $date = null) {
-        if($date == null) {
+    public function getTeacherLessons(int $teacher_id, string $date = null)
+    {
+        if ($date == null) {
             $date = date('Y-m-d');
         }
-            $day = strtolower(date('l', strtotime($date)));
+        $day = strtolower(date('l', strtotime($date)));
 
         return $this
-            ->select('timetable.id, timetable.lesson_number, lessons.title, classes.title as class, week_day')
+            ->select('timetable.id, timetable.lesson_number, lessons.title,timetable.lesson_id, classes.title as class, week_day')
             ->join('lessons', 'lessons.id = timetable.lesson_id', 'left')
             ->join('classes', 'classes.id = timetable.class_id', 'left')
             ->where('teacher_id', $teacher_id)
             ->where('week_day', $day)
             ->orderBy('lesson_number', 'ASC')
             ->findAll();
-        }
+    }
+
+    public function getLessonStudents($lesson_id)
+    {
+        return $this
+            ->select('timetable.id, timetable.lesson_number, lessons.title,users.firstname, users.lastname, timetable.lesson_id, students.class_id, students.user_id,classes.title as class, week_day')
+            ->join('lessons', 'lessons.id = timetable.lesson_id', 'left')
+            ->join('classes', 'classes.id = timetable.class_id', 'left')
+            ->join('students', 'students.id = classes.id')
+            ->join('users','users.id = students.user_id')
+            ->where('lessons.id', $lesson_id)
+        ->orderBy('lesson_number', 'ASC')
+        ->findAll();
+    }
 
 }
 
